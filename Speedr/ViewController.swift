e5872bd137f072d0519cebb046378870b513da16
Andrew Odintsov <a1covker@gmail.com>
2017-05-17 16:05:37 +0100

    Initial Commit

---
+++ Speedr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Speedr
+//
+//  Created by Andrew Odintsov on 17/05/2017.
+//  Copyright © 2017 Andrew Odintsov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
